16da7c3a1e4f051f19b3fb495b65e0fa3f0f30d6
Stephen Barr (Vertex Development) <stephen.barr@devfactory.com>
2022-07-01 18:28:40 +0000

    Making genData take arguments

--- genData.py
+++ genData.py
@@ -4,21 +4,11 @@ import mysql.connector
 import string
 import random
 from itertools import islice
+import argparse
 
-cnx = mysql.connector.connect(user='root',
-                              password='mypass',
-                              host='127.0.0.1',
-                              database='employees'
-                             )
 
-query = "SELECT * FROM employees limit 10"
-cursor = cnx.cursor()
-cursor.execute(query, ())
 
-y = list(cursor)
-
-## Creaate a table to pad out some data
-
+## SQL to create extra data table
 employee_extra_data_SQL = """
 CREATE TABLE employee_extra (
     emp_no      INT      NOT NULL,
@@ -42,6 +32,7 @@ def printTablesList(tlist):
         print(f"{i}: {t}")
 
 letters = string.ascii_letters
+
 def makeRandomString(slen):
     return(''.join(random.choice(letters) for i in range(slen)))
 
@@ -53,18 +44,6 @@ def getEmployeeIds(cursor):
 def group_elements(lst, chunk_size):
     lst = iter(lst)
     return iter(lambda: tuple(islice(lst, chunk_size)), ())
-
-
-tables_pre = getTables(cursor)
-
-if 'employee_extra' not in tables_pre:
-    print("Missing employee_extra table. Inserting.")
-    cursor.execute(employee_extra_data_SQL, ())
-    r = list(cursor)
-
-
-tables_post = getTables(cursor)
-empIds = getEmployeeIds(cursor)
 
 def mkRandExtraData(N, 
                     empIds,
@@ -92,14 +71,37 @@ def mkRandExtraData(N,
     print(f"DONE: Inserted {ins_count}/{N}")
 
 
-mkRandExtraData(1337, empIds, cnx, cursor)
 
-mkRandExtraData(31337, empIds, cnx, cursor)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num", help='Number of sample entries', type=int, required=True)
+    args = parser.parse_args()
+    print(args)
 
-mkRandExtraData(543210, empIds, cnx, cursor)
+    cnx = mysql.connector.connect(user='root',
+                                password='mypass',
+                                host='127.0.0.1',
+                                database='employees'
+                                )
 
-# Calling this function with this argument may not be a good time
-# mkRandExtraData(8675309, empIds, cnx, cursor)
 
-cnx.close()
+    # Make the new table if necessary
+    cursor = cnx.cursor()
+    tables_pre = getTables(cursor)
+    if 'employee_extra' not in tables_pre:
+        print("Missing employee_extra table. Inserting.")
+        cursor.execute(employee_extra_data_SQL, ())
+        r = list(cursor)
+    else:
+        print("employee_extra table present.")
+        
+    tables_post = getTables(cursor)
+
+    empIds = getEmployeeIds(cursor)
+
+    num_samples = args.num
+    print(f"Called with {num_samples}")
+    mkRandExtraData(num_samples, empIds, cnx, cursor)
+    cnx.close()
+
 
