3886cf0f4acc343907b3a1d93453c6919f09409d
Stephen Barr (Vertex Development) <stephen.barr@devfactory.com>
2022-07-06 23:58:17 +0000

    Modifying backup and restore to use mydumper/myloader

--- README.md
+++ README.md
@@ -41,3 +41,29 @@ gunzip -c edb1/mysql-data/2022-07-03_204450__backup.sql.gz | docker exec -i mysq
 ```sh
 docker exec mysql-server /usr/bin/mysqladmin -uroot -pmypass shutdown
 ```
+
+## Dumper/Loader
+
+```sh
+mydumper -h 127.0.0.1 -u root  --password=mypass --database=employees --threads=8  --outputdir edb1/mysql-data/
+```
+
+```sh
+myloader -h 127.0.0.1 -u root  --password=mypass --database=employees --threads=8 --directory edb1/mysql-data/2022-07-06_231402
+```
+
+## MySQL Fast write mode
+
+```
+docker run -d --name=mysql-server -p 3306:3306 \
+        -e MYSQL_ROOT_PASSWORD=mypass       \
+        -e MYSQL_DATABASE=testdb            \
+        -v mysql-data:/var/lib/mysql        \
+        mysql:5.7.38 --max-allowed-packet=67108864 \
+        --innodb_buffer_pool_size=4G \
+        --innodb_log_buffer_size=256M \
+        --innodb_log_file_size=1G \
+        --innodb_write_io_threads=16 \
+        --innodb_flush_log_at_trx_commit=0 \
+        --innodb-doublewrite=0
+```
--- backupDatabaseToS3.sh
+++ backupDatabaseToS3.sh
@@ -4,14 +4,15 @@
 #docker exec mysql-server /usr/bin/mysqladmin -uroot -pmypass shutdown
 #echo "...DONE"
 
-echo "Archiving up Docker volume..."
+echo "Archiving Database..."
 #./docker-backup-volume/backup-volume.sh -v mysql-data -p edb1
-docker exec mysql-server /usr/bin/mysqldump -u root --password=mypass employees | gzip -9 -c > `date +"%Y-%m-%d_%H%M%S"`__backup.sql.gz
+#docker exec mysql-server /usr/bin/mysqldump -u root --password=mypass employees | gzip -9 -c > `date +"%Y-%m-%d_%H%M%S"`__backup.sql.gz
+dname=`date +"%Y-%m-%d_%H%M%S"`
+mkdir -p edb1/mysql-data/$dname
+mydumper -h 127.0.0.1 -u root  --password=mypass --database=employees \
+         --threads=7 --chunk-filesize 512 \
+        --outputdir edb1/mysql-data/$dname
 echo "...DONE"
 
-echo "Archive information:"
-export FNAME=edb1/mysql-data/`ls -t edb1/mysql-data/ | head -1`
-ls -lh $FNAME
-
 echo "Syncing to S3"
-aws s3 sync edb1/ s3://devgraph-devspaces-demos/dockerdb/edb1
+aws s3 sync edb1/mysql-data/$dname s3://devgraph-devspaces-demos/dockerdb/edb1/mysql-data/$dname
